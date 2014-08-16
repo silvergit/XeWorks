2501501d04bda33a645d394df7858243831f41e2
Ali Reza Pazhouhesh <pazhouhesh.ali@gmail.com>
2014-08-16 20:58:23 +0430

    Commit 5

---
+++ The Equivalent LATEX and TEX Commands/حاشیه چپ سه.js
@@ -0,0 +1,8 @@
+// TeXworksScript
+// Title: حاشیه چپ سه
+// Author: Ali Reza Pazhouhesh
+// Version: 0.1
+// Date: 08/16/2014
+// Script-Type: standalone
+
+TW.target.insertText("\\leftmarginiii");
---
+++ The Equivalent LATEX and TEX Commands/لاتک.js
@@ -0,0 +1,8 @@
+// TeXworksScript
+// Title: لاتک
+// Author: Ali Reza Pazhouhesh
+// Version: 0.1
+// Date: 08/16/2014
+// Script-Type: standalone
+
+TW.target.insertText("\\LaTeX");
